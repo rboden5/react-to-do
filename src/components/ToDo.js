657afad7e5b096277952158972a492ee9f767224
Your Name <ryanboden5@gmail.com>
2019-03-07 16:02:34 -0800

    RTD Components Checkpoint Completed

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,12 +1,9 @@
-import React, { Component } from 'react';
+ import React, { Component } from 'react';
 
  class ToDo extends Component {
    render() {
      return (
-        <li>
-         <input type="checkbox" checked={ this.props.isCompleted } />
-         <span>{ this.props.description }</span>
-       </li>
+       <li>a todo will go here</li>
      );
    }
  }
